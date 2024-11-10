77237b58a1d36a9a90c58a5f9cad110c8f7137ed
Pusoski Hristijan <82514752+pusoskii@users.noreply.github.com>
2024-11-10 18:52:40 +0100

    build: add main file

---
+++ main.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+from filters.filter_one.filter_one_main import run_filter_one
+from filters.filter_three.filter_three_main import run_filter_three
+from database.setup_database import get_database, initialize_collections
+import time
+from datetime import datetime
+
+
+def main():
+    initialize_collections()
+
+    db = get_database()
+    if db.issuers.find_one() is None:
+        run_filter_one()
+
+    run_filter_three()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--format", required=True, type=int)
+    args = parser.parse_args()
+    os.environ['FORMAT_DATA'] = str(args.format)
+
+    start_time = time.time()
+    main()
+    elapsed_time = (time.time() - start_time) / 60
+    now = datetime.now()
+    today = f"{now.month}/{now.day}/{now.year}"
+
+    db = get_database()
+    db.scrapings.insert_one({
+        "scraping_date": today,
+        "elapsed_time_minutes": elapsed_time,
+    })
