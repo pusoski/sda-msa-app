4f23d6cf6e048ad74bf7143ea67fbb372e810259
iskra <iskra.stojchevska@students.finki.ukim.mk>
2024-11-10 18:33:06 +0100

    feat: add filter files

---
+++ homework_one/filter_three/filter_three_main.py
@@ -0,0 +1,12 @@
+from database.setup_database import get_database
+from homework_one.filter_two.filter_two_main import scrape_data_for_issuer, get_last_scraped_date
+
+
+def run_filter_three():
+    db = get_database()
+    issuers = db.issuers.find({"valid": True})
+
+    for issuer in issuers:
+        symbol = issuer["symbol"]
+        scraping_date = get_last_scraped_date(symbol)
+        scrape_data_for_issuer(symbol, scraping_date)
