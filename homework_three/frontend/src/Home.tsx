b2b49baf46e4fc6529db2cc24c1df8e003d750eb
Pusoski Hristijan <82514752+pusoskii@users.noreply.github.com>
2024-12-10 13:07:28 +0100

    build: add basic page info for get_started

--- homework_three/frontend/src/Home.tsx
+++ homework_three/frontend/src/Home.tsx
@@ -1,9 +1,9 @@
 const Home = () => {
     return (
-        <div>
-            <h1>SDA-MSE APP</h1>
-            <p>You are currently viewing a prototype of the application.</p>
-            <p>This is a very early version used for demonstrating purposes of the tech stack.</p>
+        <div className="content-container">
+            <div className="page-title-left">
+                <h1>Get Started</h1>
+            </div>
         </div>
     );
 };
