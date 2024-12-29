a223614aa05a57583013e367fcab0cf7331d369c
Irinej Ilievski <irinejilievski@gmail.com>
2024-12-29 18:24:39 +0100

    Update 1

--- homework_three/frontend/src/Home.tsx
+++ homework_three/frontend/src/Home.tsx
@@ -1,55 +1,63 @@
-import "./assets/css/Home.css"
+import "./assets/css/Home.css";
+
 const Home = () => {
     return (
-        <div className="content-container">
-            <div className="page-title-left">
-                <h1> Get Started</h1>
-            </div>
-            <div className='page-content'>
-                <p>
-                    Welcome to the SDA-MSA, or Stock data analysis app for the Macedonian stock exchange.
-                    This is the starting page of a architecture and software design course from FINKI.
-                </p>
-                <hr id="break"/>
-                <p>
-                    <i>Note ! <br/> </i>
-                    <u>The information and publications of this web application do not constitute financial advice</u>,
-                    investment advice, trading advice or any other form of advice.
-                    All results from the web application are intended for information purposes only.
-                    <br/>
-                    <br/>
-                    It is very important to do your own analysis before making any investment based on your own personal
-                    circumstances.<br/>
-                    If you need financial advice or further advice in general, it is recommended that you identify a
-                    relevantly qualified individual in your jurisdiction who can advise you accordingly.
-                    <hr id="break"/>
-                </p>
-                <p>
-                    This app scrapes raw data from the Macedonian stock exchange, after reformatting certain aspects
-                    about the data, such as filtering out un-wanted issuers and symbols,
-                    it checks if the scraped data is valid, and provides easy visibility to you, the end user.
-                    <br/>
+        <main className="content-container">
+            <header className="page-header">
+                <h1>Get Started</h1>
+            </header>
 
-                    <ul> From here you may navigate to the:
-                        <li>Issuers List, to view all issuers, their state and date of scrape. </li>
-                        <li>The Data Analyses tab, that displays selected issuer, current, live and historical data.
+            <section className="welcome-section">
+                <p>
+                    Welcome to <strong>MSA</strong>, the Stock Data Analysis application for the Macedonian Stock Exchange.
+                    This platform is developed as part of the Software Design and Architecture FCSE Skopje, aiming to provide
+                    comprehensive tools for stock data analysis.
+                </p>
+
+                <aside className="disclaimer">
+                    <div>
+                        <strong>Important Notice:</strong>
+                        <p>
+                            The information and publications provided by this web application do not constitute financial advice,
+                            investment advice, trading advice, or any other form of advice. All results are intended for informational
+                            purposes only.
+                        </p>
+                        <p>
+                            It is crucial to conduct your own analysis before making any investment decisions based on your personal
+                            circumstances. If you require financial or general advice, we recommend consulting a qualified professional
+                            in your jurisdiction.
+                        </p>
+                    </div>
+                </aside>
+
+            </section>
+
+            <section className="app-overview">
+                <article>
+                    <p>
+                        MSA efficiently scrapes raw data from the Macedonian Stock Exchange, reformats key aspects such as
+                        filtering, validates the scraped data, and presents it in an easily accessible format for users.
+                        It provides navigation using the following sections:</p>
+                    <ul className="features-list">
+                        <li>
+                            <strong>Issuers List:</strong> View all issuers, their status, and the date of data
+                            scraping.
                         </li>
-                        <li>Personalization tab, where we you can choose various preferences such as hide certain
-                            issuers, data display and chart types
+                        <li>
+                            <strong>Data Analyses:</strong> Calculate and visualize technical indicators (moving
+                            averages and oscillators).
                         </li>
-                        <li>And the Data Reports Tab where you can view detailed generated reports from the scraped
-                            data
+                        <li>
+                            <strong>Data Reports:</strong> View detailed reports with buy, hold and sell signalization.
+                        </li>
+                        <li>
+                            <strong>Personalization:</strong> Customize your experience with issuers watchlist and
+                            custom page layouts.
                         </li>
                     </ul>
-
-                    <ul>This application was built by:
-                        <li>Hristijan Pusovski</li>
-                        <li>Iskra Stojcevksa</li>
-                        <li>Irinej Ilievksi</li>
-                    </ul>
-                </p>
-            </div>
-        </div>
+                </article>
+            </section>
+        </main>
     );
 };
 
